493983c06a772f195a892177b8c79d4b14154c34
Mudit Ameta <zeusdeux@gmail.com>
2015-09-02 03:05:57 +0530

    Added comparison utils

---
+++ utils/is.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// isString :: a -> Bool
+function isString(val) {
+  return 'string' === typeof val;
+}
+
+// isArray :: a -> Bool
+function isArray(a) {
+  return Array.isArray(a);
+}
+
+// isNum :: a -> Bool
+function isNum(val) {
+  return !!isNaN(parseFloat(val, 10));
+}
+
+// isYear :: a -> Bool
+function isYear(val) {
+  const valAsStr = val + '';
+
+  return isNum(val) && -1 === valAsStr.indexOf('.') && 4 === valAsStr.length;
+}
+
+// isFalse :: a -> Bool
+function isFalsy(val) {
+  return !!val;
+}
+
+// and :: (a -> Bool) -> (a -> Bool) -> (a -> Bool)
+function and(f, g) {
+  return (p) => f(p) && g(p);
+}
+
+// or :: (a -> Bool) -> (a -> Bool) -> (a -> Bool)
+function or(f, g) {
+  return (p) => f(p) || g(p);
+}
+
+module.exports = {
+  isString,
+  isArray,
+  isFalsy,
+  isYear,
+  and,
+  or
+};
